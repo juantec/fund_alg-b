c8073882c9dc1081907cd7a30b71ac02b80f3c94
juantec <2109146@upy.edu.mx>
2022-01-28 09:30:54 -0600

    estructura gemeral c

---
+++ unit1/GeneralStrucProgramC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+
+//Global Variables;
+//Create a variable: dataType nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "JuanJose";
+char unCaracter = 'M'; 
+////////////////////////////
+
+
+/// La ejecucion del programa//
+; int main (){
+    printf("Hello Data B %s\n", name );
+    return 0;
+}    
+//////////////////////////////
