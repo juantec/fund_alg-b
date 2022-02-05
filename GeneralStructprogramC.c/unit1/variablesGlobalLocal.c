cf30be2cc9e8975ea93153cf8612f0f2be92fa98
juantec <2109146@upy.edu.mx>
2022-02-04 23:31:40 -0600

    local variables internal main

---
+++ GeneralStructprogramC.c/unit1/variablesGlobalLocal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+//Global Variables;
+char* date = "02-02-22T10:25:00z"
+////////////////////////////
+
+/// La ejecucion del programa//
+; int main (){
+   
+    return 0;
+}    
+//////////////////////////////
