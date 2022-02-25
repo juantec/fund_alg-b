229b17ecb248aadd09010dbfbab6ff47a9f1276d
juantec <2109146@upy.edu.mx>
2022-02-25 08:45:12 -0600

    loops

--- unit1/even_odd.c
+++ unit1/even_odd.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-int main(){
-  int numberTotest  = 10;
-  int result = numberTotest % 2;
-  bool hasModule = ( result > 0);
 
-  if (hasModule) {
-      printf("odd");
-  } else {
-      printf("Pair"); 
-  }
+int main(int argc, char** argv){
+
+    if (argc != 2) {
+        printf("Error program: U need to insert a number, e.g: ./%s argv[1]\n", argv[0] );
+        return 0;
+    }
+
+    bool isANumber = (argv[1][0] >= 48 && argv[1][0] <= 57);
+
+    if ( !isANumber)  {
+        printf("Error program: U need to insert a number, e.g: %s 8\n", argv[0] );
+        return 0;
+    }
+
+    int numberToTest = atoi(argv[1]);
+    int result = numberToTest % 2;
+    bool hasModule = (result > 0);
+
+    if (hasModule) {
+        printf("The number is odd\n");
+
+    } else {
+        printf("The number is even\n");
+        
+    }
+    return 0;
 }
-
-//
-//g++ even_odd.c  -o pairtask
-// ./pairtask
--- unit1/par.c
+++ unit1/par.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <curses.h>
+#include <stdbool.h>
 
 main()
 { 
